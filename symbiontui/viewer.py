c9b55471645e4d154b014be0bdb596443d3431f4
SymbiontConjurer <135414057+SymbiontConjurer@users.noreply.github.com>
2023-06-03 00:45:55 -0700

    Move template to new file

--- symbiontui/viewer.py
+++ symbiontui/viewer.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string, send_from_directory, send_file
+from flask import Flask, request, render_template, send_from_directory, send_file
 import os
 import argparse
 import imghdr
@@ -57,129 +57,18 @@ def index():
                     key, value = chunk[1].split(b'\x00')
                     png_chunks[key.decode()] = value.decode()
 
-    return render_template_string("""
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Image Viewer</title>
-    <style>
-    body {
-        background-color: #111111;
-        color: #ffffff;
-        line-height: 1.5;
-    }
-
-    .column {
-        float: left;
-        width: 33.33%;
-        overflow: auto;  /* Add scrollbar to the image list */
-        height: 90vh;
-    }
-
-    .row::after {
-        content: "";
-        clear: both;
-        display: table;
-    }
-
-    img {
-        width: 100%;
-        height: auto;
-    }
-
-    h2, h3, p {
-        color: #ffffff;
-    }
-
-    a {
-        color: #57d8ff;
-        text-decoration: none;
-    }
-
-    a:visited {
-        font-weight: bold;
-        color: #2b6374;
-    }
-    
-    a:hover {
-        text-decoration: underline;
-    }
-    </style>
-    <script>
-    document.onkeydown = function(e) {
-        switch (e.keyCode) {
-            case 37:
-                // left arrow key
-                {% if prev_image %}
-                window.location.href = '/?dir={{current_dir}}&image={{prev_image}}';
-                {% endif %}
-                break;
-            case 39:
-                // right arrow key
-                {% if next_image %}
-                window.location.href = '/?dir={{current_dir}}&image={{next_image}}';
-                {% endif %}
-                break;
-        }
-    };
-    </script>
-</head>
-<body>
-<div class="row">
-    <div class="column">
-        <h2>Directories</h2>
-        <ul>
-            {% if current_dir != image_dir %}
-            <li><a href="/?dir={{current_dir}}/..">...</a></li>
-            {% endif %}
-            {% for directory in directories %}
-            <li><a href="/?dir={{current_dir}}/{{directory}}">{{directory}}</a></li>
-            {% endfor %}
-        </ul>
-        <h2>Images</h2>
-        <ul>
-            {% for image in images %}
-            <li><a href="/?dir={{current_dir}}&image={{image}}">{{image}}</a></li>
-            {% endfor %}
-        </ul>
-    </div>
-    <div class="column">
-        {% if selected_image %}
-        <h2>{{selected_image}}</h2>
-        <img src="/image?dir={{current_dir}}&image={{selected_image}}" alt="Selected Image">
-        <div>
-            <a href="/download?dir={{current_dir}}&image={{selected_image}}">Save</a>
-            {% if prev_image %}
-            <a href="/?dir={{current_dir}}&image={{prev_image}}">Prev</a>
-            {% endif %}
-            {% if next_image %}
-            <a href="/?dir={{current_dir}}&image={{next_image}}">Next</a>
-            {% endif %}
-        </div>
-        {% else %}
-        <p>No image selected.</p>
-        {% endif %}
-    </div>
-    <div class="column">
-        <h2>Image Metadata</h2>
-        {% if image_metadata %}
-        <p>Size: {{image_metadata.st_size}} bytes</p>
-        <p>Created: {{image_metadata.st_ctime}}</p>
-        <p>Modified: {{image_metadata.st_mtime}}</p>
-        {% endif %}
-        {% if png_chunks %}
-        <h2>PNG Chunks</h2>
-        {% for key, value in png_chunks.items() %}
-        <h3>{{key}}</h3>
-        <p>{{value}}</p>
-        {% endfor %}
-        {% endif %}
-    </div>
-</div>
-</body>
-</html>
-    """, directories=directories, images=images, selected_image=selected_image, image_metadata=image_metadata, current_dir=current_dir, next_image=next_image, prev_image=prev_image, png_chunks=png_chunks, image_dir=app.config['image_dir'])
-
+    return render_template(
+        "index.html",
+        directories=directories,
+        images=images,
+        selected_image=selected_image,
+        image_metadata=image_metadata,
+        current_dir=current_dir,
+        next_image=next_image,
+        prev_image=prev_image,
+        png_chunks=png_chunks,
+        image_dir=app.config['image_dir'],
+    )
 @app.route('/image')
 def serve_image():
     image_path = request.args.get('image')
