6701ec0112e0d43116b92cba31c49f70a6dc7ca0
SymbiontConjurer <135414057+SymbiontConjurer@users.noreply.github.com>
2023-06-03 03:25:57 -0700

    Refactor

--- symbiontui/image_repository.py
+++ symbiontui/image_repository.py
@@ -1,40 +1,37 @@
+from dataclasses import dataclass
+from typing import List
 import os
 import imghdr
-from dataclasses import dataclass
-from typing import List, Optional, Set
-
 
 @dataclass
-class ImageData:
+class Image:
     name: str
     path: str
     tags: List[str]
     category: str
 
-
 class ImageRepository:
     def __init__(self, directory):
         self.directory = directory
 
-    def list(self, category: Optional[str] = None):
+    def _get_image_category(self, image_path: str) -> List[str]:
+        # For now, all images have the 'image' category
+        return ['image']
+
+    def list(self, category: str = None) -> List[Image]:
         files_and_dirs = os.listdir(self.directory)
         images = sorted(
-            [
-                ImageData(
-                    name=f,
-                    path=os.path.join(self.directory, f),
-                    tags=[],
-                    category="image",
-                )
-                for f in files_and_dirs
-                if os.path.isfile(os.path.join(self.directory, f))
-                and imghdr.what(os.path.join(self.directory, f))
-                and (category is None or category == "image")
-            ],
+            [Image(f, os.path.join(self.directory, f), [], self._get_image_category(os.path.join(self.directory, f))) 
+            for f in files_and_dirs 
+            if os.path.isfile(os.path.join(self.directory, f)) and imghdr.what(os.path.join(self.directory, f))],
             key=lambda x: os.stat(x.path).st_ctime,
             reverse=True  # Descending order
         )
+        
+        if category:
+            images = [image for image in images if category in image.category]
+            
         return images
 
-    def categories(self) -> Set[str]:
-        return {"images"}
+    def categories(self) -> set:
+        return {"image"}
