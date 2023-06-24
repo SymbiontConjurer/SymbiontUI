49a583df79e061140dc1373eaa424b3e94286f24
SymbiontConjurer <135414057+SymbiontConjurer@users.noreply.github.com>
2023-06-24 00:53:08 -0700

    Start working on torchserve stuff

---
+++ py/server/pack/stable_diffusion.py
@@ -0,0 +1,9 @@
+import torch
+from diffusers import StableDiffusionPipeline
+
+model_id = "runwayml/stable-diffusion-v1-5"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+
+prompt = "a photo of an astronaut riding a horse on mars"
+image = pipe(prompt).images[0]  
